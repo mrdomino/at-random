f0074c5863bb04d66f4732eeee1f03a14f12a9d0
Steve Dee <steve@smartercode.net>
2012-05-16 21:00:24 -0700

    App.run returns 0 on success
    - (minor) 'a successful run' is a description, not a context.
    
    - Describe a successful run with sample argument values straight off.
      This might be overspecifying, but that's probably fine given that we
      wrote the API in the README.
    
    - Move 'picks a time' into the successful run block.
    
    - Provide stubs for `AtCmd` and `PickTime`.

--- lib/at-random/app.rb
+++ lib/at-random/app.rb
@@ -1,7 +1,8 @@
 module AtRandom
   class App
-    def self.run
+    def self.run(argv)
       AtCmd.new(PickTime.new.time_s)
+      0
     end
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,24 +3,33 @@ require 'spec_helper'
 require 'at-random/app'
 
 module AtRandom
-  class AtCmd; end
-  class PickTime; end
+  class AtCmd
+    def initialize(*args); end
+  end
+
+  class PickTime
+    def time_s; end
+  end
 end
 
 describe AtRandom::App do
   describe '#run' do
-    subject { AtRandom::App.run }
+    describe 'a successful run' do
+      subject do
+        argv = %w[--random-seed 20 --from=12:34 --to=13 -q q ls /home/steve]
+        AtRandom::App.run(argv)
+      end
 
-    let(:picked_time) { '12:34' }
+      it 'picks a time to pass to `at`' do
+        picked_time = '12:35'
+        AtRandom::PickTime.any_instance.expects(:time_s).returns(picked_time)
+        AtRandom::AtCmd.expects(:new).with(picked_time, any_parameters)
+        subject
+      end
 
-    it 'picks a time to pass to `at`' do
-      AtRandom::PickTime.any_instance.expects(:time_s).returns(picked_time)
-      AtRandom::AtCmd.expects(:new).with(picked_time, any_parameters)
-      subject
-    end
-
-    context 'a successful run' do
-      it 'returns zero'
+      it 'returns zero' do
+        subject.should eq 0
+      end
     end
 
     context 'when something goes wrong' do
