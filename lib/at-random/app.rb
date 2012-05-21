25bd36cd9cd8c221805d871f90d5aaedae7158f2
Steve Dee <steve@smartercode.net>
2012-05-21 13:13:42 -0700

    Sanify App API

--- lib/at-random/app.rb
+++ lib/at-random/app.rb
@@ -3,27 +3,29 @@ require 'at-random/pick_time'
 
 module AtRandom
   class App
-    def self.run(argv)
-      pick_time_opts = {}
+    def initialize(argv)
+      @pick_time_opts = {}
       while argv[0] =~ /--(from|to|random-seed)/
         if argv[0] =~ /--from/
           from_str = argv[0][7..-1]
-          pick_time_opts[:from] = from_str
+          @pick_time_opts[:from] = from_str
         elsif argv[0] =~ /--to/
           to_str = argv[0][5..-1]
-          pick_time_opts[:to] = to_str
+          @pick_time_opts[:to] = to_str
         elsif argv[0] =~ /--random-seed/
-          random_seed = argv[0][14..-1]
-          Kernel.srand(random_seed.to_i)
+          @random_seed = argv[0][14..-1].to_i
         end
         argv.shift
       end
 
-      at_args = argv
-      picked_time = PickTime.new pick_time_opts
+      @at_args = argv
+    end
 
+    def run
+      Kernel.srand(@random_seed) if @random_seed
+      @picked_time = PickTime.new @pick_time_opts
       begin
-        AtCmd.new(picked_time.time_s, at_args)
+        AtCmd.new(@picked_time.time_s, @at_args)
         0
       rescue Exception => e
         $stderr.puts e.message
--- spec/at-random/app_spec.rb
+++ spec/at-random/app_spec.rb
@@ -3,21 +3,21 @@ require 'spec_helper'
 require 'at-random/app'
 
 describe AtRandom::App do
-  describe '.run' do
+  let!(:picked_time) { AtRandom::PickTime.new }
+
+  before do
+    Kernel.stubs(:srand)
+
+    AtRandom::AtCmd.stubs(:new)
+
+    AtRandom::PickTime.stubs(:new).returns(picked_time)
+    AtRandom::PickTime.any_instance.stubs(:time_s)
+  end
+
+  describe '#run' do
     def run_with_good_args
       argv = %w[--random-seed 20 --from=12:34 --to=13 -q q ls /home/steve]
-      AtRandom::App.run(argv)
-    end
-
-    let!(:picked_time) { AtRandom::PickTime.new }
-
-    before do
-      Kernel.stubs(:srand)
-
-      AtRandom::AtCmd.stubs(:new)
-
-      AtRandom::PickTime.stubs(:new).returns(picked_time)
-      AtRandom::PickTime.any_instance.stubs(:time_s)
+      AtRandom::App.new(argv).run
     end
 
     context 'with good arguments' do
@@ -58,7 +58,7 @@ describe AtRandom::App do
     end
 
     context 'without arguments' do
-      subject { AtRandom::App.run }
+      subject { AtRandom::App.new.run }
 
       it 'displays usage information'
 
@@ -67,85 +67,85 @@ describe AtRandom::App do
         subject.should be > 0
       end
     end
+  end
 
-    describe 'arguments' do
-      def run_with_arg(arg)
-        argv = [arg, 'ls']
-        AtRandom::App.run(argv)
+  describe '.new' do
+    def run_with_arg(arg)
+      argv = [arg, 'ls']
+      AtRandom::App.new(argv).run
+    end
+
+    shared_examples_for 'time argument' do
+      it 'accepts HH' do
+        run_with_arg("#{subject}=23").should eq 0
       end
 
-      shared_examples_for 'time argument' do
-        it 'accepts HH' do
-          run_with_arg("#{subject}=23").should eq 0
-        end
+      it 'accepts HH:MM' do
+        run_with_arg("#{subject}=01:23").should eq 0
+      end
+    end
 
-        it 'accepts HH:MM' do
-          run_with_arg("#{subject}=01:23").should eq 0
-        end
+    def run_with_time_arg(arg, time_s)
+      AtRandom::PickTime.expects(:new).
+        with(arg.to_sym => time_s).
+        returns(picked_time)
+      run_with_arg("--#{arg}=#{time_s}")
+    end
+
+    describe '--from' do
+      subject { '--from' }
+
+      it_behaves_like 'time argument'
+
+      it 'passes valid times to PickTime' do
+        run_with_time_arg 'from', '10:00'
+
+        run_with_time_arg 'from', '22:33'
       end
 
-      def run_with_time_arg(arg, time_s)
+      it 'expands "HH" to "HH:00"'
+    end
+
+    describe '--to' do
+      subject { '--to' }
+
+      it_behaves_like 'time argument'
+
+      it 'passes to PickTime' do
+        run_with_time_arg 'to', '11:00'
+
+        run_with_time_arg 'to', '23:59'
+      end
+
+      it 'expands "HH" to "HH:59"'
+    end
+
+    describe '--from and --to' do
+      specify 'both get passed to PickTime' do
         AtRandom::PickTime.expects(:new).
-          with(arg.to_sym => time_s).
+          with(:from => '23:05', :to => '23:04').
           returns(picked_time)
-        run_with_arg("--#{arg}=#{time_s}")
+        AtRandom::App.new(%w[--from=23:05 --to=23:04]).run
+      end
+    end
+
+    describe '--random-seed' do
+      it 'seeds the rng' do
+        Kernel.expects(:srand).with(12345)
+        run_with_arg('--random-seed=12345')
+      end
+    end
+
+    describe '`at` args' do
+      specify 'get passed to `at`' do
+        AtRandom::AtCmd.expects(:new).with(anything, %w[-q a echo hi])
+        AtRandom::App.new(%w[-q a echo hi]).run
       end
 
-      describe '--from' do
-        subject { '--from' }
-
-        it_behaves_like 'time argument'
-
-        it 'passes valid times to PickTime' do
-          run_with_time_arg 'from', '10:00'
-
-          run_with_time_arg 'from', '22:33'
-        end
-
-        it 'expands "HH" to "HH:00"'
-      end
-
-      describe '--to' do
-        subject { '--to' }
-
-        it_behaves_like 'time argument'
-
-        it 'passes to PickTime' do
-          run_with_time_arg 'to', '11:00'
-
-          run_with_time_arg 'to', '23:59'
-        end
-
-        it 'expands "HH" to "HH:59"'
-      end
-
-      describe '--from and --to' do
-        specify 'both get passed to PickTime' do
-          AtRandom::PickTime.expects(:new).
-            with(:from => '23:05', :to => '23:04').
-            returns(picked_time)
-          AtRandom::App.run %w[--from=23:05 --to=23:04]
-        end
-      end
-
-      describe '--random-seed' do
-        it 'seeds the rng' do
-          Kernel.expects(:srand).with(12345)
-          run_with_arg('--random-seed=12345')
-        end
-      end
-
-      describe '`at` args' do
-        specify 'get passed to `at`' do
-          AtRandom::AtCmd.expects(:new).with(anything, %w[-q a echo hi])
-          AtRandom::App.run(%w[-q a echo hi])
-        end
-
-        describe 'other `at-random` args' do
-          specify "aren't passed to `at`" do
-            AtRandom::AtCmd.expects(:new).with(anything, %w[-m true])
-            AtRandom::App.run(%w[--from=10:00 --to=12:00 -m true])
-          end
+      describe 'other `at-random` args' do
+        specify "aren't passed to `at`" do
+          AtRandom::AtCmd.expects(:new).with(anything, %w[-m true])
+          AtRandom::App.new(%w[--from=10:00 --to=12:00 -m true]).run
         end
       end
     end
