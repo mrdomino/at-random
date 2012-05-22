72b82be1df21af27ae48d6c9a74f64f5bb29c235
Steve Dee <steve@smartercode.net>
2012-05-21 20:07:55 -0700

    Implemented questionable PickTime spec

--- lib/at-random/pick_time.rb
+++ lib/at-random/pick_time.rb
@@ -1,10 +1,34 @@
 module AtRandom
   class PickTime
+    class ImpossibleRange < Exception; end
+
     def initialize(opts={})
+      from_h, from_m = parse_time(opts[:from] || '00:00')
+      to_h, to_m = parse_time(opts[:to] || '23:59')
+      now_h, now_m = parse_time(Time.now.strftime('%H:%M'))
+
+      if now_h >= from_h
+        from_h = now_h
+        if now_m > from_m
+          from_m = now_m
+        end
+      end
+
+      if now_h > to_h || (now_h == to_h && now_m > to_m)
+        raise ImpossibleRange, 'Asked for :to sooner than now'
+      end
+
+      @hour = rand(to_h - from_h) + from_h
+      @minute = rand(to_m - from_m) + from_m
     end
 
     def time_s
-      '13:35'
+      format("%02d:%02d", @hour, @minute)
+    end
+
+  private
+    def parse_time(time_s)
+      time_s.split(':').map(&:to_i)
     end
   end
 end
--- spec/at-random/pick_time_spec.rb
+++ spec/at-random/pick_time_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 require 'at-random/pick_time'
 
 describe AtRandom::PickTime do
-  let(:frozen_now) { Time.at(0) }
+  let(:frozen_now) { Time.local(1970, 1, 1, 0, 0) }
 
   before do
     Time.stubs(:now).returns(frozen_now)
@@ -13,38 +13,41 @@ describe AtRandom::PickTime do
     it 'is a time string' do
       subject.time_s.should =~ /[012][0-9]:[0-5][0-9]/
     end
+
+    it 'is a function of Kernel.rand' do
+      (1..20).map do |i|
+        Kernel.srand(i)
+        AtRandom::PickTime.new.time_s
+      end.uniq.length.should be_within(1).of(19)
+    end
+
+    it 'is the same every time for a given PickTime' do
+      subject.time_s.should eq subject.time_s
+    end
   end
 
   describe '.new' do
-    context 'without arguments' do
-      it 'works'
-    end
-
     context 'with :from => "00:34"' do
       subject { AtRandom::PickTime.new :from => "00:34" }
 
       its(:time_s) { should be > '00:34' }
 
-      context 'at 12:36pm local' do
-        let(:frozen_now) { Time.at(Time.local(2012, 05, 16, 12, 36)) }
+      context 'at 17:36pm local' do
+        let(:frozen_now) { Time.at(Time.local(2012, 05, 16, 17, 36)) }
 
-        its(:time_s) { should be > '12:36' }
+        its(:time_s) { should be > '17:36' }
       end
     end
 
     context 'with :to => "12:34"' do
       subject { AtRandom::PickTime.new :to => '12:34' }
 
-      its(:time_s) {
-        pending
-        should be <= '12:34' }
+      its(:time_s) { should be <= '12:34' }
 
       context 'at 12:36pm local' do
         let(:frozen_now) { Time.local(2012, 05, 16, 12, 36) }
 
-        it {
-          pending
-          lambda { subject }.should raise_error }
+        it { lambda { subject }.should raise_error }
       end
     end
   end
