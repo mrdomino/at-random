feac05153d670936483a6f54aa8d955a99aa358b
Steve Dee <steve@smartercode.net>
2012-05-21 21:09:06 -0700

    Specify and implement AtCmd

--- lib/at-random/at_cmd.rb
+++ lib/at-random/at_cmd.rb
@@ -1,4 +1,24 @@
 module AtRandom
   class AtCmd
+    class InvalidTime < Exception; end
+
+    def initialize(*args)
+      if args[0]
+        timespec = args.shift
+        if !(timespec =~ /[0-2][0-9]:[0-5][0-9]/)
+          raise ArgumentError
+        end
+
+        if timespec < Time.now.strftime('%H:%M')
+          raise InvalidTime, "Timespec #{timespec} is earlier than now"
+        end
+
+        @exec_args = ['at', timespec, args].flatten
+      end
+    end
+
+    def exec
+      Process.exec(*@exec_args)
+    end
   end
 end
--- spec/at-random/at_cmd_spec.rb
+++ spec/at-random/at_cmd_spec.rb
@@ -3,5 +3,82 @@ require 'spec_helper'
 require 'at-random/at_cmd'
 
 describe AtRandom::AtCmd do
-  it 'is unspecified'
+  let!(:frozen_now) { Time.local(2015, 05, 25) }
+  before do
+    Time.stubs(:now).returns(frozen_now)
+    Process.stubs(:exec)
+  end
+
+  let(:timespec) { '12:30' }
+  let(:at_args) { [] }
+
+  let(:at_with_args) { AtRandom::AtCmd.new(timespec, at_args) }
+
+  describe '.new' do
+    subject { at_with_args }
+
+    describe 'timespec argument' do
+      context 'a valid time string' do
+        let(:timespec) { '12:34' }
+
+        context 'after now' do
+          let(:frozen_now) { Time.local(2012, 05, 25, 12, 33) }
+
+          it { lambda { subject }.should_not raise_error }
+        end
+
+        context 'before now' do
+          let(:frozen_now) { Time.local(2012, 05, 25, 12, 35) }
+
+          it { lambda { subject }.
+            should raise_error AtRandom::AtCmd::InvalidTime }
+        end
+      end
+
+      context 'a non-time string' do
+        let(:timespec) { 'as:df' }
+
+        it { lambda { subject }.should raise_error ArgumentError }
+      end
+
+      context 'a non-string' do
+        let(:timespec) { Time.local(2012, 05, 25, 12, 32) }
+
+        it { lambda { subject }.should raise_error ArgumentError }
+      end
+    end
+
+    describe 'at_args argument' do
+      context 'an array of `at` args' do
+        let(:at_args) { ['-f', 'foo', '-mv'] }
+
+        it { lambda { subject }.should_not raise_error }
+      end
+    end
+  end
+
+  describe '#exec' do
+    subject { at_with_args.exec }
+
+    it 'execs `at`' do
+      Process.expects(:exec).with('at', any_parameters)
+      subject
+    end
+
+    context 'with at args' do
+      let(:at_args) { ['-f', 'foo'] }
+
+      it 'passes arguments to `at`' do
+        Process.expects(:exec).with('at', anything, '-f', 'foo')
+        subject
+      end
+    end
+
+    describe '`at` timespec' do
+      it 'is the first argument to AtCmd.new' do
+        Process.expects(:exec).with('at', timespec, any_parameters)
+        subject
+      end
+    end
+  end
 end
